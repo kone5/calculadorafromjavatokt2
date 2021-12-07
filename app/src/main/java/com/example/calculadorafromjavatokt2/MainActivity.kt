8be7479a8b455162f2fac20a2508127292ef4cce
kone <sergioamj5@gmail.com>
2021-12-07 11:31:52 -0300

    segundo commit

--- app/src/main/java/com/example/calculadorafromjavatokt2/MainActivity.kt
+++ app/src/main/java/com/example/calculadorafromjavatokt2/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.TextView
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.example.calculadorafromjavatokt2.R
 
 class MainActivity : AppCompatActivity() {
@@ -20,31 +21,48 @@ class MainActivity : AppCompatActivity() {
         resultado = findViewById(R.id.resultado)
     }
 
-    fun sumar(view: View?) {
-        val resultadoNumero =
-            cifra_01!!.text.toString().toInt() + cifra_02!!.text.toString().toInt()
-        resultado!!.text = resultadoNumero.toString() + ""
-        Log.d("operacionSuma", "Boton suma apretado")
+    fun sumar (view: View?) {
+        if(cifra_01!!.text.toString() != "" && cifra_02!!.text.toString() != "") {
+            val resultadoNumero = cifra_01!!.text.toString().toInt() + cifra_02!!.text.toString().toInt()
+            resultado!!.text = resultadoNumero.toString()
+        } else {
+            Toast.makeText(this, "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show()
+            Log.d("ERROR Suma", "Algun campo esta vacio")
+        }
     }
 
-    fun restar(view: View?) {
-        val resultadoNumero =
-            cifra_01!!.text.toString().toInt() - cifra_02!!.text.toString().toInt()
-        resultado!!.text = resultadoNumero.toString() + ""
-        Log.d("operacionResta", "Boton resta apretado")
+    fun restar (view: View?) {
+        if(cifra_01!!.text.toString() != "" && cifra_02!!.text.toString() != "") {
+            val resultadoNumero = cifra_01!!.text.toString().toInt() - cifra_02!!.text.toString().toInt()
+            resultado!!.text = resultadoNumero.toString()
+        }else {
+            Toast.makeText(this, "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show()
+            Log.d("ERROR Resta", "Algun campo esta vacio")
+        }
     }
 
-    fun multiplicar(view: View?) {
-        val resultadoNumero =
-            cifra_01!!.text.toString().toInt() * cifra_02!!.text.toString().toInt()
-        resultado!!.text = resultadoNumero.toString() + ""
-        Log.d("operacionMult", "Boton multiplicacion apretado")
+    fun multiplicar (view: View?) {
+        if(cifra_01!!.text.toString() != "" && cifra_02!!.text.toString() != "") {
+            val resultadoNumero = cifra_01!!.text.toString().toInt() * cifra_02!!.text.toString().toInt()
+            resultado!!.text = resultadoNumero.toString()
+        }else {
+            Toast.makeText(this, "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show()
+            Log.d("ERROR Multiplicacion", "Algun campo esta vacio")
+        }
     }
 
-    fun dividir(view: View?) {
-        val resultadoNumero =
-            cifra_01!!.text.toString().toInt() / cifra_02!!.text.toString().toInt()
-        resultado!!.text = resultadoNumero.toString() + ""
-        Log.d("operacionDiv", "Boton division apretado")
+    fun dividir (view: View?) {
+        if(cifra_01!!.text.toString() != "" && cifra_02!!.text.toString() != "") {
+            if(cifra_02!!.text.toString() != "0") {
+                val resultadoNumero = cifra_01!!.text.toString().toInt() / cifra_02!!.text.toString().toInt()
+                resultado!!.text = resultadoNumero.toString()
+            }else{
+                Toast.makeText(this, "No es posible dividir por cero", Toast.LENGTH_SHORT).show()
+                Log.d("ERROR en la Dicision", "Valor cero en el divisor")
+            }
+        }else {
+            Toast.makeText(this, "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show()
+            Log.d("ERROR Division", "Algun campo esta vacio")
+        }
     }
 }
